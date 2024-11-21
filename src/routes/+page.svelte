fd8b536187d2b7135b1685a57d4819320a9ec287
培根是條狗 <jack@dezu.group>
2024-11-21 18:11:04 +0800

    feat: init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<svelte:head>
+    <title>培根是條狗 | @bacon4dog</title>
+    <meta charset="utf-8" />
+    <meta name="language" content="zh-TW" />
+    <meta name="author" content="@bacon4dog" />
+    <meta name="robots" content="index, follow" />
+    <meta name="description" content="培根是條狗 | @bacon4dog" />
+    <meta name="keywords" content="培根是條狗, bacon4dog" />
+    <link rel="canonical" href="https://bacon4dog.dev" />
+    <meta property="og:url" content="https://bacon4dog.dev" />
+    <meta property="og:title" content="培根是條狗 | @bacon4dog" />
+    <meta property="og:description" content="培根是條狗 | @bacon4dog" />
+    <meta property="og:type" content="website" />
+    <meta property="og:author" content="https://github.com/bacon4dog" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="培根是條狗 | @bacon4dog" />
+    <meta name="twitter:description" content="培根是條狗 | @bacon4dog" />
+</svelte:head>
+
+<div class="grid place-items-center w-full h-full">
+	<h1 class="text-4xl font-bold text-white animate-bounce">
+		<a href="https://github.com/bacon4dog">培根是條狗</a>
+	</h1>
+</div>
+
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import extractorSvelte from '@unocss/extractor-svelte'
+import UnoCSS from 'unocss/vite'
+
+/** @type {import('vite').UserConfig} */
+export default defineConfig({
+	plugins: [
+		UnoCSS({
+			extractors: [
+				extractorSvelte(),
+			],
+		}),
+		sveltekit()
+	]
+});
