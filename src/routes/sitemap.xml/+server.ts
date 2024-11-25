66b648804a90194aed3fb1bd52e6a7a337a75170
培根是條狗 <jack@dezu.group>
2024-11-25 09:52:10 +0800

    feat: add sitemap, deps upgrade

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,40 @@
+import type { RequestHandler } from '@sveltejs/kit';
+
+type Route = {
+    path: string;
+    lastmod?: string;
+    changefreq?: 'always' | 'hourly' | 'daily' | 'weekly' | 'monthly' | 'yearly' | 'never';
+    priority?: number;
+};
+
+const routes: Route[] = [
+    {
+        path: '/',
+        changefreq: 'weekly',
+        priority: 1.0
+    }
+];
+
+export const GET: RequestHandler = async ({ url }) => {
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${routes
+        .map(
+            (route) => `
+    <url>
+        <loc>${url.origin}${route.path}</loc>
+        <lastmod>${route.lastmod || new Date().toISOString()}</lastmod>
+        ${route.changefreq ? `<changefreq>${route.changefreq}</changefreq>` : ''}
+        ${route.priority ? `<priority>${route.priority}</priority>` : ''}
+    </url>`
+        )
+        .join('')}
+</urlset>`.trim();
+
+    return new Response(sitemap, {
+        headers: {
+            'Content-Type': 'application/xml',
+            'Cache-Control': 'max-age=3600'
+        }
+    });
+}; 
